bbdb7fe411087486a79e61690ffa290d6402c148
Andrius <Andriusj6@hotmail.com>
2021-07-12 08:03:14 +0100

    tests running

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <Router>
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
         <App />
-      </Router>
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-)
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
